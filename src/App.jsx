4f8c44e817b05844d133af15c3cb9c225a3a5751
himarnoel <olaimarnoel@gmail.com>
2022-11-26 20:19:11 +0100

    nav for lg done"

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,21 @@ function App() {
 
   return (
     <div className="">
-      <nav className="flex  w-screen shadow-lg py-3">
+      <nav className="flex  w-screen shadow-lg py-3 px-3 justify-between items-center">
         <div className="flex items-center" id="logo">
-          <img src={logo} alt="" className="object-contain w-24" />
-          <p className="text-[#FD8C00] text-xl font-[]">
+          <img src={logo} alt="logo" className="object-contain w-20" />
+          <p className="text-[#FD8C00] text-[1.2rem] font-[]">
             Landmark University Evangelism Team
           </p>
+        </div>
+        <p className="text-bold text-[#FD8C00] text-3xl">Attendance System </p>
+        <div className="flex justify-between ">
+          <button className="bg-[#FD8C00] p-3 rounded  text-white hover:bg-[#e27e03] mx-2">
+            Join the team
+          </button>
+          <button className="bg-[#FD8C00] p-3 rounded  text-white hover:bg-[#e27e03]">
+            Join the team
+          </button>
         </div>
       </nav>
       <div className="p-20 "></div>
