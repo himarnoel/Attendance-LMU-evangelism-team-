8eeb86df9199748814fd3c7144634d2101d00117
himarnoel <olaimarnoel@gmail.com>
2022-12-30 19:54:06 +0100

    added button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='h-screen w-screen'>
-      
+    <div className="h-screen w-screen flex justify-center items-center flex-col">
+      <button className="bg-[#FD8C00] p-2 lg:w-[20rem] rounded  text-white hover:bg-[#fda335] mb-20">
+        Join Evangelism Unit
+      </button>
+      <button className="bg-[#FD8C00] p-2  lg:w-[20rem]  rounded  text-white hover:bg-[#fda335]">
+        Admin Login
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
